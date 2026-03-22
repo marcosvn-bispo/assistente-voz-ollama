3bc13fe9e4f9b6452d7c3a06b4a7ede1a1bbf350
marcosvn-bispo <marcosvnbp@gmail.com>
2026-03-22 17:20:17 -0300

    Substituindo o uso do pyaudio por sounddevice

--- src/speech.py
+++ src/speech.py
@@ -1,16 +1,32 @@
+import sounddevice as sd
+import numpy as np
 import speech_recognition as sr
+import scipy.io.wavfile as wav
 
-def listen():
+def listen(duration=5, fs=16000):
+    print("🎤 Ouvindo...")
+
+    # grava áudio
+    recording = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype='int16')
+    sd.wait()
+
+    filename = "temp_audio.wav"
+    wav.write(filename, fs, recording)
+
     recognizer = sr.Recognizer()
 
-    with sr.Microphone() as source:
-        print("🎤 Ouvindo...")
-        audio = recognizer.listen(source)
-
     try:
+        with sr.AudioFile(filename) as source:
+            audio = recognizer.record(source)
+
         text = recognizer.recognize_google(audio, language="pt-BR")
         print(f"Você disse: {text}")
         return text
+
     except sr.UnknownValueError:
         print("Não entendi.")
+        return None
+
+    except Exception as e:
+        print(f"Erro: {e}")
         return None
--- src/tts.py
+++ src/tts.py
@@ -1,16 +1,8 @@
-import speech_recognition as sr
+import pyttsx3
 
-def listen():
-    recognizer = sr.Recognizer()
+engine = pyttsx3.init()
 
-    with sr.Microphone() as source:
-        print("🎤 Ouvindo...")
-        audio = recognizer.listen(source)
-
-    try:
-        text = recognizer.recognize_google(audio, language="pt-BR")
-        print(f"Você disse: {text}")
-        return text
-    except sr.UnknownValueError:
-        print("Não entendi.")
-        return None
+def speak(text):
+    print(f"Assistente: {text}")
+    engine.say(text)
+    engine.runAndWait()
